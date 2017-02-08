b1ba363d7b81a5000cfad9b7e93776a9fda4635b
MFrJ <marcelofernaj@gmail.com>
2017-02-08 13:32:30 -0200

    start of macro function
    TODO make it not trigger on itself

--- jbot.py
+++ jbot.py
@@ -12,18 +12,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith('!jeff'):
-        await client.send_message(message.channel, "My nama jeff")
+    if message.content.startswith('!macro'):
+        s = message.content[7:]
+        if (',') not in s:
+            await client.send_message(message.channel, "!macro: <key>,<txt>")
+        else :
+            l = s.split(",")
+            await client.send_message(message.channel, ">".join(l))
+    # if message.content.startswith('!test'):
+    #     counter = 0
+    #     tmp = await client.send_message(message.channel, 'Calculating messages...')
+    #     async for log in client.logs_from(message.channel, limit=100):
+    #         if log.author == message.author:
+    #             counter += 1
+    #
+    #     await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    # elif message.content.startswith('!sleep'):
+    #     await asyncio.sleep(5)
+    #     await client.send_message(message.channel, 'Done sleeping')
+    # elif message.content.startswith('!jeff'):
+    #     await client.send_message(message.channel, "My nama jeff")
 
 client.run("Mjc4ODYzMTA3NzU2MTMwMzA0.C3y1Ig.oKNoMuPhV0EUuC5TreOgBM8yUUo")
