65c601d1b25864c6dd0a1b5278b43a54f0bc618a
MFrJ <marcelofernaj@gmail.com>
2017-02-14 12:26:54 -0200

    Trying to get YT player to work

--- jbot.py
+++ jbot.py
@@ -1,9 +1,12 @@
 import asyncio
 import discord
+import youtube_dl
 import re
 from os import path
 
 client = discord.Client()
+
+
 command = re.compile("!([a-zA-Z0-9])+")
 macro_dict = {}
 with open("C:\\Users\\Marcelo\\Desktop\\JEFFbot\\resources\\txts\\macros.txt",'r') as f:
@@ -24,20 +27,27 @@ async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
-    print('------')
+    print(discord.opus.is_loaded())
+    # for i in client.get_all_channels():
+    #     print(i.name + ":" + i.id)
 
 @client.event
 async def on_message(message):
     if not isMe(message):
         if message.content.startswith('!jeff'):
-            await client.send_message(message.channel, "My nama jeff", tts=True)
+            voice = await client.join_voice_channel(client.get_channel('278863693134168076'))
+            player = await voice.create_ytdl_player('https://www.youtube.com/watch?v=AfIOBLr1NDU')
+            player.start()
+
+
         #macro list
         elif message.content.startswith('!memes'):
             with  open("C:\\Users\\Marcelo\\Desktop\\JEFFbot\\resources\\txts\\macros.txt",'r') as f:
                 for lines in f:
                     (k,v) = lines.split(" ", 1)
                     await client.send_message(message.channel, k + ": " + v)
-        #create macro            
+
+        #create macro
         elif message.content.startswith("!"):
             if command.match(message.content):
                 if command.match(message.content).group(0)[1:] in macro_dict.keys():
